ae1e8e81769a8c7195ac5f82c50c8239f59a8b45
adilzhanshukenov <shukenovadilzhan@gmail.com>
2026-06-24 18:17:33 +0500

    more settings

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,7 @@ const nextConfig: NextConfig = {
     // Limit the build to 1 worker thread to save memory
     cpus: 1,
   },
+  assetPrefix: process.env.NODE_ENV === "production" ? "/public/" : "",
 };
 
 export default nextConfig;
