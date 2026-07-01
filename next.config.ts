6f4d4fb6ec7b7d6e6760bf469e6f6afe330e4bc0
adilzhanshukenov <shukenovadilzhan@gmail.com>
2026-07-01 17:52:44 +0500

    try to fix routing

--- next.config.ts
+++ next.config.ts
@@ -15,7 +15,15 @@ const nextConfig: NextConfig = {
   },
   output: "standalone",
   // Change the internal asset prefix path so it doesn't map to a protected folder name
-  assetPrefix: process.env.NODE_ENV === "production" ? "/static" : undefined,
+  // assetPrefix: process.env.NODE_ENV === "production" ? "/static" : undefined,
+  async rewrites() {
+    return [
+      {
+        source: "/_next/static/:path*",
+        destination: "/static/_next/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
