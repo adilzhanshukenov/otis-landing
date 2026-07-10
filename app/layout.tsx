275fcc99b240f0c0d15625ebca9c98bc7f09bc94
adilzhanshukenov <shukenovadilzhan@gmail.com>
2026-07-10 11:23:31 +0500

    gtm added

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,6 +30,23 @@ export default function RootLayout({
       className={`${robotoCondensed.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5LG4JMWJ');`}
+        </Script>
+
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5LG4JMWJ"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
         <Script id="meta-pixel-base" strategy="afterInteractive">
           {`!function(f,b,e,v,n,t,s)
 {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
